cc251eaf87082fe64481ad9512c2fef55ac8f017
CreeParker <hsgw.c.aym.98@i.softbank.jp>
2018-03-23 01:19:34 +0900

    アップデート v0.3

--- src/APITools/APITools.php
+++ src/APITools/APITools.php
@@ -72,165 +72,34 @@ class APITools extends PluginBase{
 
 	public function changeOnCommand($dir_main, $name){
 		$source = file_get_contents($dir_main);
-		$strlen = strlen($source);
-		for($a = 0; $a < $strlen; ++$a){
-			if($source[$a] == 'f' or $source[$a] == 'F'){
-			if($source[$a + 1] == 'u' or $source[$a + 1] == 'U'){
-			if($source[$a + 2] == 'n' or $source[$a + 2] == 'N'){
-			if($source[$a + 3] == 'c' or $source[$a + 3] == 'C'){
-			if($source[$a + 4] == 't' or $source[$a + 4] == 'T'){
-			if($source[$a + 5] == 'i' or $source[$a + 5] == 'I'){
-			if($source[$a + 6] == 'o' or $source[$a + 6] == 'O'){
-			if($source[$a + 7] == 'n' or $source[$a + 7] == 'N'){
-			if($source[$a + 8]== ' '){
-			for($b = $a + 9; $b < $strlen; ++$b){
-				if($source[$b] != ' '){
-				if($source[$b] == 'o'){
-				if($source[$b + 1] == 'n'){
-				if($source[$b + 2] == 'C'){
-				if($source[$b + 3] == 'o'){
-				if($source[$b + 4] == 'm'){
-				if($source[$b + 5] == 'm'){
-				if($source[$b + 6] == 'a'){
-				if($source[$b + 7] == 'n'){
-				if($source[$b + 8] == 'd'){
-				for($c = $b + 9; $c < $strlen; ++$c){
-					if($source[$c] != ' '){
-					if($source[$c] == '('){
-					for($d = $c + 1; $d < $strlen; ++$d){
-						if($source[$d] == ')'){
-						for($e = $d + 1; $e < $strlen; ++$e){
-							if($source[$e] != ' '){
-							if($source[$e] == '{'){
-								$function[] = [$a, $e];
-								break 4;
-							}else break 4;
-							}
-						}
-						}
-					}
-					}else break 2;
-					}
-				}
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
+		preg_match_all('/function\s+oncommand\s*\([^)]*\)\s*{/ui', $source, $function);
+		foreach($function[0] as $value){
+			$strlen = strlen($value);
+			if($strlen >= 32 and $strlen <= 132)
+				$rtrim[] = rtrim($value, '{').' : bool{';
 		}
-		if(isset($function)){
-			foreach($function as $value){
-				$strlen = ($value[1] - $value[0]) + 1;
-				if($strlen >= 32 and $strlen <= 130){
-					$string = [];
-					for($a = $value[0]; $a <= $value[1]; ++$a) 
-						$string[] = $source[$a];
-					$implode[] = implode('', $string);
-				}
-			}
-			if(isset($implode)){
-				$replace_function = str_replace('{', ' : bool{', $implode);
-				$replace_source = str_replace($implode, $replace_function, $source);
-				file_put_contents($dir_main, $replace_source);
-				$this->logger->info("§e[APITools] '$name'の'onCommand'を古い型から新しい型にしました");
-			}
+		if(isset($rtrim)){
+			$replace = str_replace($function[0], $rtrim, $source);
+			file_put_contents($dir_main, $replace);
+			$this->logger->info("§e[APITools] '$name'の'onCommand'を古い型から新しい型にしました");
 		}
 	}
 
 	public function changeOnRun($dir_main, $name){
 		$source = file_get_contents($dir_main);
-		$strlen = strlen($source);
-		for($a = 0; $a < $strlen; ++$a){
-			if($source[$a] == 'f' or $source[$a] == 'F'){
-			if($source[$a + 1] == 'u' or $source[$a + 1] == 'U'){
-			if($source[$a + 2] == 'n' or $source[$a + 2] == 'N'){
-			if($source[$a + 3] == 'c' or $source[$a + 3] == 'C'){
-			if($source[$a + 4] == 't' or $source[$a + 4] == 'T'){
-			if($source[$a + 5] == 'i' or $source[$a + 5] == 'I'){
-			if($source[$a + 6] == 'o' or $source[$a + 6] == 'O'){
-			if($source[$a + 7] == 'n' or $source[$a + 7] == 'N'){
-			if($source[$a + 8]== ' '){
-			for($b = $a + 9; $b < $strlen; ++$b){
-				if($source[$b] != ' '){
-				if($source[$b] == 'o'){
-				if($source[$b + 1] == 'n'){
-				if($source[$b + 2] == 'R'){
-				if($source[$b + 3] == 'u'){
-				if($source[$b + 4] == 'n'){
-				for($c = $b + 5; $c < $strlen; ++$c){
-					if($source[$c] != ' '){
-					if($source[$c] == '('){
-					for($d = $c + 1; $d < $strlen; ++$d){
-						if($source[$d] != ' '){
-						if($source[$d] != 'i'){
-						if($source[$d + 1] != 'n'){
-						if($source[$d + 2] != 't'){
-						for($e = $d + 3; $e < $strlen; ++$e){
-							if($source[$e] == ')'){
-							for($f = $e + 1; $f < $strlen; ++$f){
-								if($source[$f] == '{'){
-									$function[] = [$a, $f];
-									break 5;
-								}
-							}
-							}
-						}
-						}else break 3;
-						}else break 3;
-						}else break 3;
-						}
-					}
-					}else break 2;
-					}
-				}
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}else break;
-				}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
-			}
+		preg_match_all('/function\s+onrun\s*\(\s*(?!int)[^)]*\)\s*{/ui', $source, $function);
+		foreach($function[0] as $value){
+			$strlen = strlen($value);
+			if($strlen >= 22 and $strlen <= 122){
+				$strpos_1 = strpos($value, '(');
+				$strpos_2 = strpos($value, '$');
+				$replace_function[] = substr_replace($value, '(int ', $strpos_1, $strpos_2 - $strlen);
 			}
 		}
-		if(isset($function)){
-			foreach($function as $value){
-				$strlen = ($value[1] - $value[0]) + 1;
-				if($strlen >= 22 and $strlen <= 80){
-					$string = [];
-					for($a = $value[0]; $a <= $value[1]; ++$a) 
-						$string[] = $source[$a];
-					$implode[] = implode('', $string);
-				}
-			}
-			if(isset($implode)){
-				$replace_function = str_replace('(', '(int ', $implode);
-				$replace_source = str_replace($implode, $replace_function, $source);
-				file_put_contents($dir_main, $replace_source);
-				$this->logger->info("§e[APITools] '$name'の'onRun'を古い型から新しい型にしました");
-			}
+		if(isset($replace_function)){
+			$replace_source = str_replace($function[0], $replace_function, $source);
+			file_put_contents($dir_main, $replace_source);
+			$this->logger->info("§e[APITools] '$name'の'onRun'を古い型から新しい型にしました");
 		}
 	}
 
